ab00a745425519c238a5d920fb8ccb7e5d91b9c8
Alkihis <alkihis@gmail.com>
2020-05-25 14:01:43 +0200

    Better typings for WorkerChild

--- src/WorkerChild.ts
+++ src/WorkerChild.ts
@@ -1,39 +1,21 @@
 import { parentPort, workerData } from 'worker_threads';
 import { WorkerToMessage, WorkerTaskMessage, TASK_MESSAGE, REQUEST_END_MESSAGE, WorkerFailMessage, FAIL_MESSAGE, WorkerSuccessMessage, SUCCESS_MESSAGE, WORKER_READY } from './globals';
 
-export interface WorkerChildOptions {
-  onTask(data: any): any | Promise<any>;
-  onStartup?: (data?: any) => any | Promise<any>;
-}
-
-export class WorkerChild {
-  protected waiting_tasks: WorkerTaskMessage<any>[] = [];
+export class WorkerChild<TaskData = any, TaskResult = any, StartupData = any> {
+  protected waiting_tasks: WorkerTaskMessage<TaskData>[] = [];
   protected ready = false;
   protected init_error: any;
   protected running_tasks = new Set<string>();
 
   constructor(
-    protected options: WorkerChildOptions
+    protected options: {
+      onTask(data: TaskData): TaskResult | Promise<TaskResult>;
+      onStartup?: (data?: StartupData) => any | Promise<any>;
+    }
   ) {}
 
   isStarted(id: string) {
     return this.running_tasks.has(id);
-  }
-
-  protected postFailMessage(id: string, error: any) {
-    parentPort!.postMessage({
-      id,
-      type: FAIL_MESSAGE,
-      error
-    } as WorkerFailMessage);
-  }
-
-  protected postEndMessage(id: string, data: any) {
-    parentPort!.postMessage({
-      id,
-      type: SUCCESS_MESSAGE,
-      data
-    } as WorkerSuccessMessage<any>);
   }
 
   protected async init() {
@@ -58,7 +40,7 @@ export class WorkerChild {
     });
   }
 
-  protected async runTask(data: WorkerTaskMessage<any>) {
+  protected async runTask(data: WorkerTaskMessage<TaskData>) {
     if (!this.ready) {
       this.waiting_tasks.push(data);
       return;
@@ -82,12 +64,28 @@ export class WorkerChild {
 
     this.running_tasks.delete(data.id);
   }
+  
+  protected postFailMessage(id: string, error: any) {
+    parentPort!.postMessage({
+      id,
+      type: FAIL_MESSAGE,
+      error
+    } as WorkerFailMessage);
+  }
+
+  protected postEndMessage(id: string, data: TaskResult) {
+    parentPort!.postMessage({
+      id,
+      type: SUCCESS_MESSAGE,
+      data
+    } as WorkerSuccessMessage<TaskResult>);
+  }
 
   async listen() {
     parentPort!.on('message', (data: WorkerToMessage) => {
       if (data.type === TASK_MESSAGE) {
         // Worker stat message
-        this.runTask(data as WorkerTaskMessage<any>);
+        this.runTask(data as WorkerTaskMessage<TaskData>);
       }
       else if (data.type === REQUEST_END_MESSAGE) {
         this.running_tasks.delete(data.id);
